0f2e7e8968df3a419172ce066db609335c3f7282
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-17 19:55:12 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,20 +2,3 @@ require 'pry'
 
 class String
 
-  def sentence?
-    "HI!".end_with?("!")
-  => true
-  end
-
-  def question?
-
-  end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
-
-  end
-end
